e6ac3b888295138342141d82b2c8d3750e76c9bf
Martin Kavík <martin@kavik.cz>
2020-07-20 12:47:20 +0200

    navbar

--- src/lib.rs
+++ src/lib.rs
@@ -18,11 +18,16 @@ const SETTINGS: &str = "settings";
 fn init(url: Url, _: &mut impl Orders<Msg>) -> Model {
     Model {
         ctx: Context {
-            user: None,
+            // user: None,
+            user: Some(User {
+                username: "John".to_owned(),
+                email: "john@email.com".to_owned(),
+            }),
             token: None,
         },
         base_url: url.to_base_url(),
         page: Page::Home,
+        menu_visible: false,
     }
 }
 
@@ -34,6 +39,7 @@ struct Model {
     ctx: Context,
     base_url: Url,
     page: Page,
+    menu_visible: bool,
 }
 
 struct Context {
@@ -68,7 +74,7 @@ impl<'a> Urls<'a> {
         self.base_url().add_path_part(CLIENTS_AND_PROJECTS)
     }
     fn time_tracker(self) -> Url {
-        self.base_url().add_path_part(CLIENTS_AND_PROJECTS)
+        self.base_url().add_path_part(TIME_TRACKER)
     }
     fn time_blocks(self) -> Url {
         self.base_url().add_path_part(TIME_BLOCKS)
@@ -84,11 +90,13 @@ impl<'a> Urls<'a> {
 
 enum Msg {
     UrlChanged(subs::UrlChanged),
+    ToggleMenu,
 }
 
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
         Msg::UrlChanged(url) => {},
+        Msg::ToggleMenu => model.menu_visible = not(model.menu_visible),
     }
 }
 
@@ -97,7 +105,131 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 // ------ ------
 
 fn view(model: &Model) -> Node<Msg> {
-    div!["Root view"]
+    view_navbar(model.menu_visible, &model.base_url, model.ctx.user.as_ref())
+}
+
+fn view_navbar(menu_visible: bool, base_url: &Url, user: Option<&User>) -> Node<Msg> {
+    nav![
+        C!["navbar"],
+        attrs!{
+            At::from("role") => "navigation",
+            At::AriaLabel => "main navigation",
+        },
+        view_brand_and_hamburger(menu_visible, base_url),
+        view_navbar_menu(menu_visible, base_url, user),
+    ]
+}
+
+fn view_brand_and_hamburger(menu_visible: bool, base_url: &Url) -> Node<Msg> {
+    div![
+        C!["navbar-brand"],
+        // ------ Logo ------
+        a![
+            C!["navbar-item", "has-text-weight-bold", "is-size-3"],
+            attrs!{At::Href => Urls::new(base_url).home()},
+            "TT"
+        ],
+        // ------ Hamburger ------
+        a![
+            C!["navbar-burger", "burger", IF!(menu_visible => "is-active")],
+            attrs!{
+                At::from("role") => "button",
+                At::AriaLabel => "menu",
+                At::AriaExpanded => menu_visible,
+            },
+            ev(Ev::Click, |_| Msg::ToggleMenu),
+            span![attrs!{At::AriaHidden => "true"}],
+            span![attrs!{At::AriaHidden => "true"}],
+            span![attrs!{At::AriaHidden => "true"}],
+        ]
+    ]
+}
+
+fn view_navbar_menu(menu_visible: bool, base_url: &Url, user: Option<&User>) -> Node<Msg> {
+    div![
+        C!["navbar-menu", IF!(menu_visible => "is-active")],
+        view_navbar_menu_start(base_url),
+        view_navbar_menu_end(base_url, user),
+    ]
+}
+
+fn view_navbar_menu_start(base_url: &Url) -> Node<Msg> {
+    div![
+        C!["navbar-start"],
+        a![
+            C!["navbar-item"],
+            attrs!{At::Href => Urls::new(base_url).time_tracker()},
+            "Time Tracker",
+        ],
+        a![
+            C!["navbar-item"],
+            attrs!{At::Href => Urls::new(base_url).clients_and_projects()},
+            "Clients & Projects",
+        ],
+        a![
+            C!["navbar-item"],
+            attrs!{At::Href => Urls::new(base_url).time_blocks()},
+            "Time Blocks",
+        ],
+    ]
+}
+
+fn view_navbar_menu_end(base_url: &Url, user: Option<&User>) -> Node<Msg> {
+     div![
+        C!["navbar-end"],
+        div![
+            C!["navbar-item"],
+            div![
+                C!["buttons"],
+                if let Some(user) = user {
+                    view_buttons_for_logged_in_user(base_url, user)
+                } else {
+                    view_buttons_for_anonymous_user()
+                }
+            ]
+        ]
+    ]
+}
+
+fn view_buttons_for_logged_in_user(base_url: &Url, user: &User) -> Vec<Node<Msg>> {
+    vec![
+        a![
+            C!["button", "is-primary"],
+            attrs![
+                At::Href => Urls::new(base_url).settings(),
+            ],
+            strong![&user.username],
+        ],
+        a![
+            C!["button", "is-light"],
+            attrs![
+                // @TODO: Write the correct href.
+                At::Href => "/"
+            ],
+            "Log out",
+        ]
+    ]
+}
+
+fn view_buttons_for_anonymous_user() -> Vec<Node<Msg>> {
+    vec![
+        a![
+            C!["button", "is-primary"],
+            attrs![
+                // @TODO: Write the correct href.
+                At::Href => "/"
+            ],
+            strong!["Sign up"],
+        ],
+        a![
+            C!["button", "is-light"],
+            attrs![
+                // @TODO: Write the correct href.
+                At::Href => "/"
+            ],
+            "Log in",
+        ]
+    ]
 }
 
 // ------ ------
